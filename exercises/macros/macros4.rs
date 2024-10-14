4886a5ea0a5e587ac7fcda63edb7151f86f2e194
Carbon <2779066456@qq.com>
2024-10-15 02:36:31 +0800

    feat: complete the part of macros

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,19 +3,18 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
 }
 
 fn main() {
+    // 类似于一种重载
     my_macro!();
     my_macro!(7777);
 }
